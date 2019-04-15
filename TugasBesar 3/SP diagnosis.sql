d8351eba038c932a385b369893611402086a6353
GABRIEL PANJI LAZUARDI <i16068@ftis>
2019-04-15 14:19:08 +0700

    Perbaikan sp diagnosis

--- TugasBesar 3/SP diagnosis.sql
+++ TugasBesar 3/SP diagnosis.sql
@@ -3,12 +3,14 @@ ALTER PROCEDURE Diag
 AS
 	
 	DECLARE
-	@gejala INT --Gejala yang dialami pasien
+	@idPenyakit INT, --memeriksa penyakit yang dialami
+	@minGejala INT, --minimal gejala penyakit
+	@gejalaDialami INT --gejala yang dialami
 
 	--Tabel penyakit yang mungkin dialami pasien
 	CREATE TABLE #penyakit (
-		namaPenyakit VARCHAR (258),
-		minimalGejala INT
+		id INT,
+		idGejala INT
 	)
 
 	--Tabel penyakit yang dialami pasien
@@ -16,37 +18,91 @@ AS
 		namaPenyakit VARCHAR (258),
 	)
 
+	--Table gejala pasie
+	CREATE TABLE #gejala(
+		id INT
+	)
+
 	--Mencari Jumlah Gejala yang dialami Pasien
+	INSERT INTO #gejala
 	SELECT
-		@gejala = COUNT(Record.idGejala)
+		Record.idGejala
 	FROM
 		Record
 	WHERE
 		Record.idCheckUp = @idCheckUp
 
-
-
-	--Mencari hasil checkUp berdasrkan Id checkUp
-	INSERT INTO  #penyakit
-	SELECT Distinct
-		namaDiagnosis, Penyakit.jumlahGejala
-	FROM 
-		Record 
-		join Diagnosis on Record.idGejala = Diagnosis.idGejala 
-		join Penyakit on Diagnosis.idPenyakit = Penyakit.idPenyakit
-	WHERE
-		Record.idCheckUp = @idCheckUp
-
-	--Cursor untuk memeriksa minimal gejala
-	INSERT INTO #diagnosis
+	--Cursor
+	DECLARE cariPenyakits cursor
+	FOR
 	SELECT
-		#penyakit.namaPenyakit
+		Diagnosis.idPenyakit
 	FROM
-		#penyakit
-	WHERE
-		@gejala >= #penyakit.minimalGejala
+		Diagnosis
 
-	SELECT
+	OPEN cariPenyakits
+
+	FETCH NEXT FROM
+		cariPenyakits
+	INTO
+		@idPenyakit
+
+	--Menghitung jumlah gejala pasien per penyakit
+	WHILE(@@FETCH_STATUS = 0)
+	BEGIN
+		--Me-fetch penayakit dari tabel
+		INSERT INTO #Penyakit
+		SELECT
+			Diagnosis.idPenyakit, Diagnosis.idGejala
+		FROM
+			Diagnosis
+		WHERE
+			Diagnosis.idPenyakit = @idPenyakit
+
+		--Mencari minimal gejala
+		SELECT
+			@minGejala = Penyakit.jumlahGejala
+		FROM
+			Penyakit
+		WHERE
+			Penyakit.idPenyakit = @idPenyakit
+		
+		--Menghitung gejala yang dialami pasien
+		SELECT
+			@gejalaDialami = COUNT(#gejala.id)
+		FROM
+			#Penyakit INNER JOIN #gejala on #gejala.id = #penyakit.idGejala
+
+		--memeriksa apakah gejala dialami seusai dengan minimal gejala
+		IF(@gejalaDialami >= @minGejala)
+		BEGIN
+			INSERT INTO #diagnosis
+			SELECT
+				Penyakit.namaDiagnosis
+			FROM
+				Penyakit
+			WHERE
+				Penyakit.idPenyakit = @idPenyakit
+		END
+
+		--Mengkosongkan table #penyakit
+		DELETE #penyakit
+
+		--Mengupdate Fetch next
+		FETCH NEXT FROM
+			cariPenyakits
+		INTO
+			@idPenyakit
+
+	END
+
+
+	CLOSE cariPenyakits
+	DEALLOCATE cariPenyakits
+
+
+
+	SELECT DISTINCT
 		#diagnosis.namaPenyakit
 	FROM
 		#diagnosis
