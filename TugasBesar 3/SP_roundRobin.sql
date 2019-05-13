fab06bfd7fa117396e6679114fde671e889e39b6
Muharodan <muharodan14@gmail.com>
2019-05-13 14:27:57 +0700

    Memperbaiki sp histogran

---
+++ TugasBesar 3/SP_roundRobin.sql
@@ -0,0 +1,34 @@
+CREATE PROCEDURE insRoundRobinGejala
+	@idCheckUp INT,
+	@idGejala INT
+AS
+	
+	DECLARE 
+		@baris INT -- baris yang harus diisi
+
+	--Mencari baris yang harus diisi
+	SELECT
+		@baris = config.baris
+	FROM
+		Config
+
+
+
+	--Memasukkan data ke Table
+	UPDATE Record
+	SET idCheckUp = @idCheckUp, idGejala = @idGejala
+	WHERE Baris = @baris
+
+	--Update baris pada config
+	IF(@baris = 60)
+	BEGIN
+		SET @baris = 1
+	END
+	ELSE
+	BEGIN
+		SET @baris = @baris + 1
+	END
+
+	UPDATE CONFIG
+	SET baris = @baris
+
